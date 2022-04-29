72b58ddc75f40511a849653e4ca14cef14c76e35
Seishi Isozaki <isozaki1357@gmail.com>
2022-04-29 00:07:13 +0000

    #78 refactor

--- problem78.hs
+++ problem78.hs
@@ -1,6 +1,7 @@
 import Data.List
 import Control.Monad
 import Control.Monad.ST
+import Control.Monad.Cont
 import Data.Array
 import Data.Array.ST
 
@@ -9,35 +10,31 @@ main = do
     print $ problem78
 
 problem78 :: Maybe Int
-problem78 = findIndex (\cnt -> cnt `mod` 1000000 == 0) $ elems $ runSTArray $ countExpressions 100000
-
-countExpressions :: Int -> ST s (STArray s Int Integer)
-countExpressions n = do
-    p <- pArray
-    foldM (\acc m -> loop m [0..] acc) p [1..n+1]
+problem78 = findIndex (\cnt -> cnt `mod` 1000000 == 0) $ elems $ countExpressions limit array
     where
-        pArray = newListArray (0,limit+1) $ 1:replicate (limit + 1) 0
-        loop _ [] arr = return arr
-        loop n (k:ks) arr
-            | n<mp = return arr
-            | n<mm = do
-                update' arr sign mp
-                return arr
-            | otherwise = do
-                update arr sign mp mm
-                loop n ks arr
-            where
-                mp = k * (3*k-1) `div` 2
-                mm = k * (3*k+1) `div` 2
-                sign = if k`mod`2 == 1 then 1 else -1
-                update arr sign mp mm = do
-                    addp <- readArray arr (n-mp)
-                    addm <- readArray arr (n-mm)
-                    org <- readArray arr n
-                    writeArray arr n (org + sign * (addp + addm))
-                update' arr sign mp = do
-                    addp <- readArray arr (n-mp)
-                    org <- readArray arr n
-                    writeArray arr n (org + sign * addp)
+        array = listArray (0,limit+1) $ 1:replicate (limit + 1) 0
+
+withBreak :: ((r -> ContT r (ST s) b) -> ContT r (ST s) r) -> ST s r
+withBreak = flip runContT pure . callCC
+
+countExpressions :: Int -> Array Int Integer -> Array Int Integer
+countExpressions n arr = runSTArray $ do
+    stArray <- thaw arr
+    forM_ [1..n+1] $ \m -> do
+        withBreak $ \break ->
+            forM_ [0..] $ \k -> do
+                let mp = k * (3*k-1) `div` 2
+                let mm = k * (3*k+1) `div` 2
+                let sign = if k`mod`2 == 1 then 1 else -1
+                when (m<mp) $ break ()
+                addp <- lift $ readArray stArray (m-mp)
+                org <- lift $ readArray stArray m
+                lift $ writeArray stArray m (org + sign * addp)
+                when (m<mm) $ break ()
+                addm <- lift $ readArray stArray (m-mm)
+                org <- lift $ readArray stArray m
+                lift $ writeArray stArray m (org + sign * addm)
+    return stArray
+
 limit :: Int
 limit = 100000

6f507f1f204c2767acfacdd45cd081e56aadce7e
Seishi Isozaki <isozaki1357@gmail.com>
2022-12-18 00:45:09 +0000

    refactoring #1

--- problem1.hs
+++ problem1.hs
@@ -1,12 +1,8 @@
-import qualified Data.Set as S
+import Data.List
 
 main :: IO ()
 main = do
     print $ problem1 1000
 
 problem1 :: Int -> Int 
-problem1 n = let
-    z3 = S.fromList [3,6..n-1]
-    z5 = S.fromList [5,10..n-1]
-    z = S.union z3 z5
-    in sum z
+problem1 n = sum $ union [3,6..n-1] [5,10..n-1]
